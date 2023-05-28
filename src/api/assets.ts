40a5f64892fe1d9a70a5aa407fee34edf8a01e7a
dokimion24 <leecy3610@gmail.com>
2023-05-28 20:54:58 +0900

    chore: :hammer: prettier 적용

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,17 +1,16 @@
-import { readFile } from "fs/promises";
-import path from "path";
+import { readFile } from 'fs/promises'
+import path from 'path'
 
 export interface Asset {
-  id: number;
-  title: string;
-  price: number;
-  discount: number;
-  createdAt: string;
-  updatedAt: string;
+  id: number
+  title: string
+  price: number
+  discount: number
+  createdAt: string
+  updatedAt: string
 }
 
 export async function getAllAssets(): Promise<Asset[]> {
-  const filePath = path.join(process.cwd(), "src", "data", "assets.json");
-  console.log(filePath);
-  return readFile(filePath, "utf-8").then<Asset[]>(JSON.parse);
+  const filePath = path.join(process.cwd(), 'src', 'data', 'assets.json')
+  return readFile(filePath, 'utf-8').then<Asset[]>(JSON.parse)
 }
