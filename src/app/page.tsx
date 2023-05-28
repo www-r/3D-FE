538b87bcab6272496bbf8a88402b8db32b82dbe1
dokimion24 <leecy3610@gmail.com>
2023-05-28 19:17:45 +0900

    feat: :sparkles: 메인페이지 AssetsList 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function HomePage() {
-  return <>HomePage 개별에셋 페이지</>;
+import { getAllAssets } from "@/api/assets";
+import AssetsList from "@/components/assets/AssetsList";
+
+export default async function HomePage() {
+  const assets = await getAllAssets();
+  console.log(assets);
+
+  return (
+    <section className="mx-6 mt-8">
+      <div className="flex h-8">
+        <h1>개별 에셋</h1>
+        <span>(713)</span>
+      </div>
+      <AssetsList assets={assets} />
+    </section>
+  );
 }
