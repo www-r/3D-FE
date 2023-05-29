e868ef2e88af92fe0596070af17be330940ff56b
dokimion24 <leecy3610@gmail.com>
2023-05-29 17:31:00 +0900

    feat: :sparkles: drawer 컴포넌트 생성

---
+++ src/components/common/Drawer.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { closeDrawer } from '@/store/drawerSlice'
+import { RootState } from '@/store/store'
+import { ReactNode } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+interface Props {
+  children: ReactNode
+}
+
+export default function Drawer({ children }: Props) {
+  const dispatch = useDispatch()
+  const isOpen = useSelector((state: RootState) => state.drawer.isOpen)
+  console.log(isOpen)
+
+  const handleCloseDrawer = () => {
+    dispatch(closeDrawer())
+  }
+
+  return isOpen ? (
+    <div className="w-[1209px] h-screen bg-blue-300">
+      <div onClick={handleCloseDrawer}>닫기</div>
+      <section>{children}</section>
+    </div>
+  ) : null
+}
