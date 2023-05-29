eedfee5e7a3ae17bb905cfa3dd56919943212dad
dokimion24 <leecy3610@gmail.com>
2023-05-29 17:32:23 +0900

    style: :lipstick: asset style 지정
    flex 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 import { getAllAssets } from '@/api/assets'
 import AssetsList from '@/components/assets/AssetsList'
+import Drawer from '@/components/common/Drawer'
 
 export default async function HomePage() {
   const assets = await getAllAssets()
 
   return (
-    <section className="px-6 pt-8">
-      <div className="flex h-8">
-        <h1 className="text-neutral-navy-100">개별 에셋</h1>
-        <span className="text-neutral-navy-200">(713)</span>
-      </div>
-      <AssetsList assets={assets} />
-    </section>
+    <div className="flex h-full">
+      <section className="px-6 pt-8 w-full">
+        <div className="flex h-8">
+          <h1 className="text-neutral-navy-100">개별 에셋</h1>
+          <span className="text-neutral-navy-200">(713)</span>
+        </div>
+        <AssetsList assets={assets} />
+      </section>
+      <Drawer />
+    </div>
   )
 }
