c01e5720ca06922a31822e683df799dcbe43ba64
jiyoon29 <yoon__29@naver.com>
2023-05-30 16:05:40 +0900

    style: :lipstick: layout style 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="h-14 bg-bg-1 border-b border-solid border-transparent-navy-30 ">
+    <header className="fixed z-10 w-full h-[7.2rem] bg-bg-1 border-b border-solid border-transparent-navy-30 ">
       Header
     </header>
   )
