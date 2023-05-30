04a7a06e905168f2d5323b4997b9ff537de13ef1
dokimion24 <leecy3610@gmail.com>
2023-05-30 15:56:25 +0900

    feat: :sparkles: 레이아웃 크기 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { getAllAssets } from '@/api/assets'
-import AssetDetail from '@/components/assets/AssetDetail'
+import AssetDetail from '@/components/assetDetail/AssetDetail'
 import AssetsList from '@/components/assets/AssetsList'
 import Drawer from '@/components/common/Drawer'
 
@@ -7,15 +7,15 @@ export default async function HomePage() {
   const assets = await getAllAssets()
 
   return (
-    <div className="flex h-full">
+    <div className="flex h-full ">
       <section className="px-[23px] pt-8 w-full">
         <AssetsList assets={assets} />
       </section>
-      <div className="sticky top-0 h-full">
+      <section className="sticky top-0 h-full">
         <Drawer>
-          <AssetDetail />
+          <AssetDetail/>
         </Drawer>
-      </div>
+      </section>
     </div>
   )
 }
