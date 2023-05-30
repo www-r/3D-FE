951ecf1c5b7d61ed9c5914d5b579c131923af86f
dokimion24 <leecy3610@gmail.com>
2023-05-30 16:04:05 +0900

    feat: :sparkles: assetDetailNav 폴더 경로 변경
    assetDetail/AssetDetailNav.tsx

--- src/components/assetDetail/AssetDetailNav.tsx
+++ src/components/assetDetail/AssetDetailNav.tsx
@@ -12,16 +12,16 @@ export default function AssetDetailNav() {
 
   return (
     <div className="w-full flex h-[50px] text-neutral-200">
-      <div className="w-[315px] flex justify-center items-center border-solid border-transparent-navy-30 border-b-[3px] hover:bg-bg-2 hover:border-primary-main">
+      <div className="w-[315px] flex justify-center items-center  border-transparent-navy-30 border-b-[3px] hover:bg-bg-2 hover:border-primary-main">
         <button>Asset Detail</button>
       </div>
-      <div className="w-[315px] flex justify-center items-center border-solid border-transparent-navy-30 border-b-[3px] hover:bg-bg-2 hover:border-primary-main">
+      <div className="w-[315px] flex justify-center items-center border-transparent-navy-30 border-b-[3px] hover:bg-bg-2 hover:border-primary-main">
         <button>
           Review <span>(0)</span>
         </button>
       </div>
       <div
-        className="w-[80px] flex justify-center items-center border-solid border-transparent-navy-30 border-b-[3px] hover:bg-bg-2 hover:border-primary-main"
+        className="w-[80px] flex justify-center items-center  border-transparent-navy-30 border-b-[3px] hover:bg-bg-2 hover:border-primary-main"
         onClick={handleCloseDrawer}
       >
         <button>닫기</button>
