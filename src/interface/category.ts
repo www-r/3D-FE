7420ab6226691ee0d4c528504595dac429230e85
jiyoon29 <yoon__29@naver.com>
2023-06-08 18:00:42 +0900

    feat: :sparkles: tags type 추가

--- src/interface/category.ts
+++ src/interface/category.ts
@@ -2,9 +2,11 @@ export interface Category {
   name: string
   subcategories: Subcategory[]
   totalCount: number
+  tags?: []
 }
 
 export interface Subcategory {
   name: string
   count: number
+  tags?: []
 }
