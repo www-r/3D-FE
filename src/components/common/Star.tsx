9b2c77a0c141af34664fcf9a202a69235b1003c6
dokimion24 <leecy3610@gmail.com>
2023-06-08 16:35:05 +0900

    feat: :sparkles: size에 따라 별크기 다르게 출력

--- src/components/common/Star.tsx
+++ src/components/common/Star.tsx
@@ -1,92 +1,57 @@
 import React from 'react'
 import Image from 'next/image'
-import { relative } from 'path'
 
 interface Props {
   star: number
+  size: string
 }
 
-function Star({ star }: Props) {
-  const starWidth = 2.4
+function Star({ star, size }: Props) {
+  const starSize = size === 'sm' ? 14 : 24
+  const starWidth = size === 'sm' ? 1.4 : 2.4
   const starsWidth = Math.round(starWidth * star * 100) / 100
 
+  const emptyStars = Array.from({ length: 5 }).map((_, index) => (
+    <Image
+      key={`empty-star-${index}`}
+      src="/icons/emptyStar.svg"
+      alt="star"
+      width={starSize}
+      height={starSize}
+      className=""
+    />
+  ))
+
+  const filledStars = Array.from({ length: 5 }).map((_, index) => (
+    <Image
+      key={`filled-star-${index}`}
+      src="/icons/filledStar.svg"
+      alt="star"
+      width={starSize}
+      height={starSize}
+      style={{ minWidth: `${starWidth}rem` }}
+    />
+  ))
+
   return (
-    <>
-      <div className={`flex z-10 absolute`}>
-        <Image
-          src="/icons/emptyStar.svg"
-          alt="star"
-          width={24}
-          height={24}
-          className=""
-        />
-        <Image
-          src="/icons/emptyStar.svg"
-          alt="star"
-          width={24}
-          height={24}
-          className=""
-        />
-        <Image
-          src="/icons/emptyStar.svg"
-          alt="star"
-          width={24}
-          height={24}
-          className=""
-        />
-        <Image
-          src="/icons/emptyStar.svg"
-          alt="star"
-          width={24}
-          height={24}
-          className=""
-        />
-        <Image
-          src="/icons/emptyStar.svg"
-          alt="star"
-          width={24}
-          height={24}
-          className=""
-        />
+    <div className="w-[20.2rem] flex justify-between items-center relative">
+      <span
+        className={`${
+          size === 'sm' ? 'text-[1.4rem]' : 'text-[2.6rem] font-bold text-primary-main'
+        }`}
+      >
+        {star}
+      </span>
+      <div className="w-[12rem] flex">
+        <div className={`flex z-10 absolute top-1/2 transform -translate-y-1/2`}>{emptyStars}</div>
+        <div
+          style={{ width: `${starsWidth}rem` }}
+          className={`flex z-10  absolute overflow-hidden top-1/2 transform -translate-y-1/2`}
+        >
+          {filledStars}
+        </div>
       </div>
-      <div style={{ width: `${starsWidth}rem` }} className={`flex z-10  absolute overflow-hidden`}>
-        <Image
-          src="/icons/filledStar.svg"
-          alt="star"
-          width={24}
-          height={24}
-          className=" shrink-0"
-        />
-        <Image
-          src="/icons/filledStar.svg"
-          alt="star"
-          width={24}
-          height={24}
-          className=" shrink-0"
-        />
-        <Image
-          src="/icons/filledStar.svg"
-          alt="star"
-          width={24}
-          height={24}
-          className=" shrink-0"
-        />
-        <Image
-          src="/icons/filledStar.svg"
-          alt="star"
-          width={24}
-          height={24}
-          className=" shrink-0"
-        />
-        <Image
-          src="/icons/filledStar.svg"
-          alt="star"
-          width={24}
-          height={24}
-          className=" shrink-0"
-        />
-      </div>
-    </>
+    </div>
   )
 }
 
