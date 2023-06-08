b58ac2c006b73fdf9b5eb346b41079a2348b143a
jiyoon29 <yoon__29@naver.com>
2023-06-08 18:02:38 +0900

    style: :lipstick: style w-[24.4rem] 추가 및 오타 삭제

--- src/components/common/SideNav/index.tsx
+++ src/components/common/SideNav/index.tsx
@@ -7,7 +7,7 @@ export default async function SideNav() {
   const data = await getAllCategories()
 
   return (
-    <aside className="fixed flex flex-col justify-between h-screen h min-w-[24.4rem] z-10 bg-bg-1 border-r border-solid border-transparent-navy-30 pt-[2.4rem] pb-16 text-mm">
+    <aside className="fixed flex flex-col justify-between h-screen w-[24.4rem] min-w-[24.4rem] z-10 bg-bg-1 border-r border-solid border-transparent-navy-30 pt-[2.4rem] pb-16 text-mm">
       <SideNavList category={data} />
       <div className="w-[20rem] mx-auto bg-neutral-navy-900 leading-[3.8rem]">
         <Link href="#" className="flex justify-center text-neutral-navy-200">
