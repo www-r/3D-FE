c99b237d994df67bd4252855f90c1987f1f92bfe
jiyoon29 <yoon__29@naver.com>
2023-06-08 17:59:48 +0900

    feat: :sparkles: tag 관련 slice 추가

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,12 +4,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import drawerReducer from './drawerSlice'
 import clickedAssetReducer from './clickedAssetSlice'
 import categoryReducer from './setSelectedCategory'
+import tagReducer from './tagSlice'
 
 export const store = configureStore({
   reducer: {
     drawer: drawerReducer,
     clickedAsset: clickedAssetReducer,
     category: categoryReducer,
+    tag: tagReducer,
   },
 })
 
