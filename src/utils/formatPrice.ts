c4fab11db0ac682694cc2d16560d5bb700c88b78
jiyoon29 <yoon__29@naver.com>
2023-06-12 17:18:47 +0900

    feat: :sparkles: formatPrice - 마이너스 추가 구현

--- src/utils/formatPrice.ts
+++ src/utils/formatPrice.ts
@@ -1,3 +1,5 @@
 export const formatPrice = (price: number): string => {
-  return `\u20A9 ${price.toLocaleString()}`;
-};
+  const currencySymbol = price < 0 ? '- \u20A9' : '\u20A9'
+  const absPrice = Math.abs(price)
+  return `${currencySymbol}${absPrice.toLocaleString()}`
+}
