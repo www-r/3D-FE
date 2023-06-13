2f9f81d54a693fd57f1a43326c62a42c9eaa14d6
ronieo <romelzen50@gmail.com>
2023-06-13 21:14:41 +0900

    feat: :sparkles: api 파일 경로 변경

--- src/api/category.ts
+++
@@ -1,8 +0,0 @@
-import { readFile } from 'fs/promises'
-import path from 'path'
-import { Category } from '@/interface/category'
-
-export async function getAllCategories(): Promise<Category[]> {
-  const filePath = path.join(process.cwd(), 'src', 'data', 'category.json')
-  return readFile(filePath, 'utf-8').then<Category[]>(JSON.parse)
-}
