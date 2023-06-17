7c510cb9ec680feb28fc2cdf8f131455a39fce90
dokimion24 <leecy3610@gmail.com>
2023-06-17 19:16:45 +0900

    feat: :sparkles: queryClient default 옵션 설정

--- src/reactQuery/Provider.tsx
+++ src/reactQuery/Provider.tsx
@@ -7,7 +7,13 @@ interface Props {
   children: ReactNode
 }
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30000,
+    },
+  },
+})
 
 export default function ReactQueryProvider({ children }: Props) {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
