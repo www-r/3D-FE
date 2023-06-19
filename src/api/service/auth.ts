77389ead5bd279f56996926219680a5c309ed700
ronieo <romelzen50@gmail.com>
2023-06-19 22:01:10 +0900

    feat: :sparkles: 인증: 회원가입 interface, api service 추가

--- src/api/service/auth.ts
+++ src/api/service/auth.ts
@@ -1,5 +1,10 @@
 import { axiosInstance } from '../axios'
-import { LoginRequest } from '../interface/auth'
+import {
+  EmailCheckResponseData,
+  LoginRequest,
+  RegisterEnroll,
+  WithdrawalRequest,
+} from '../interface/auth'
 
 export const login = async (account: LoginRequest) => {
   const res = await axiosInstance.post(`/login`, account)
@@ -10,4 +15,39 @@ export const getUser = async (user: number) => {
   if (!user) return null
   const res = await axiosInstance.get(`/s/user/${user}`)
   return res
+}
+
+export const logout = async () => {
+  const { data } = await axiosInstance.post('/logout')
+  return data
+}
+
+export const join = async (user: RegisterEnroll) => {
+  try {
+    const { data } = await axiosInstance.post('/signup', user)
+    console.log(data, '<<<<<<<<<<<<<<data 결과')
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const emailCheck = async (email: string) => {
+  try {
+    const data = await axiosInstance.post<EmailCheckResponseData>('/signup/duplicate', {
+      email,
+    })
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const withdrawal = async ({ userId, userData }: WithdrawalRequest) => {
+  const { email, password } = userData
+  const { data } = await axiosInstance.post(`/s/user/${userId}/withdraw`, {
+    email,
+    password,
+  })
+  return data
 }
