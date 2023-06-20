156dd31c70767b63f5f3f85ab89d8c552e034fe4
youngeun <jessicak0828@gmail.com>
2023-06-21 00:09:23 +0900

    feat: WishlistPage의 Container 만들기

---
+++ src/components/wishList/WishlistContainer.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import TableHeader from './TableHeader'
+import TableItem from './TableItem'
+import { CartIcon } from '../common/Icon/Icons'
+import { Asset } from '@/api/interface/asset'
+const assets =  [
+  {
+    "cartId": 1,
+    "asset": {
+      "assetId": 1,
+      "assetName": "뛰는 사람",
+      "price": 10000,
+      "discount": 0,
+      "discountPrice": 10000,
+      "extension": "확장자입니다.",
+      "size": 10.7
+    },
+    "orderId": 1, //주문 했음
+    "wishListId": 1 //위시리스트에 있음
+  },
+  {
+    "cartId": 2,
+    "asset": {
+      "assetId": 2,
+      "assetName": "걷는 사람",
+      "price": 10000,
+      "discount": 0,
+      "discountPrice": 10000,
+      "extension": "확장자입니다.",
+      "size": 10.7
+    },
+    "orderId": 2,
+    "wishListId": null
+  },
+]
+
+interface WishlistContainerProps {
+  text: string;
+}
+
+export default function WishlistContainer({text}:WishlistContainerProps) {
+  return (
+      <table className="mt-[3.2rem] max-w-[163rem] min-w-[91.9rem] w-[100%] text-neutral-navy-100">
+        <TableHeader text={text}/>
+        <tbody>
+          {assets.map((item)=><TableItem key={item.asset.assetId} assetId={item.asset.assetId} image={'ff'} assetName={item.asset.assetName} extension={item.asset.extension} size={item.asset.size} price={item.asset.price} icon={<CartIcon/>}/>)}
+        </tbody>
+      </table>
+
+  )
+}
