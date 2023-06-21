08fc7f6d15ded4d87c296386d0997848e8297826
jiyoon29 <yoon__29@naver.com>
2023-06-21 14:40:56 +0900

    feat: :sparkles: customCheckbox size 변경

--- src/components/common/CustomCheckbox.tsx
+++ src/components/common/CustomCheckbox.tsx
@@ -3,23 +3,27 @@ import React from 'react'
 type CustomCheckboxProps = {
   id: string
   className?: string
-  width?: number
-  height?: number
+  size?: number
   children?: React.ReactNode
 }
 
-function CustomCheckbox({
-  id,
-  className,
-  width = 3.2,
-  height = 3.2,
-  children,
-}: CustomCheckboxProps) {
+const sizes: Record<number, string> = {
+  2: 'w-[0.2rem] h-[0.2rem]',
+  4: 'w-[0.4rem] h-[0.4rem]',
+  8: 'w-[0.8rem] h-[0.8rem]',
+  12: 'w-[1.2rem] h-[1.2rem]',
+  16: 'w-[1.6rem] h-[1.6rem]',
+  24: 'w-[2.4rem] h-[2.4rem]',
+  32: 'w-[3.2rem] h-[3.2rem]',
+}
+
+function CustomCheckbox({ id, className, size = 32, children }: CustomCheckboxProps) {
+  const sizeClass = sizes[size] || ''
   return (
     <>
       <input type="checkbox" id={id} className="hidden" />
       <label htmlFor={id} className={`flex items-center ${className}`}>
-        <div className={`mx-auto h-[${height}rem] w-[${width}rem] bg-checkbox-off bg-cover`}></div>
+        <div className={`mx-auto ${sizeClass} bg-checkbox-off bg-cover`}></div>
         {children}
       </label>
     </>
