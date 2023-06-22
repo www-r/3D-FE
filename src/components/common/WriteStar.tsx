d6355b1d778e962162d8750b8d72da65d3ace7e6
dokimion24 <leecy3610@gmail.com>
2023-06-22 17:23:48 +0900

    feat: :sparkles: 평점 매기기 기능 구현

---
+++ src/components/common/WriteStar.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState, Dispatch, SetStateAction } from 'react'
+import Image from 'next/image'
+import Stars from './icon/Stars'
+import { formatRating } from '../../utils/formatRating'
+import { CreateReview } from '@/api/interface/review'
+
+interface Props {
+  reviewData: CreateReview
+  setReviewData: Dispatch<SetStateAction<CreateReview>>
+}
+
+export default function WriteStar({ reviewData, setReviewData }: Props) {
+  const ARRAY = [0, 1, 2, 3, 4]
+
+  const [clicked, setClicked] = useState([false, false, false, false, false])
+
+  const handleStarClick = (index: number) => {
+    setReviewData({ ...reviewData, rating: index + 1 })
+    let clickStates = [...clicked]
+    for (let i = 0; i < 5; i++) {
+      clickStates[i] = i <= index ? true : false
+    }
+    setClicked(clickStates)
+  }
+
+  return (
+    <section className="flex justify-center">
+      <div className="mr-[1.2rem] w-[2.1rem] text-mm">{reviewData.rating}</div>
+      <ul className="flex w-[10rem] items-center">
+        {ARRAY.map((item, index) => (
+          <li className="mr-[0.8rem]" key={index} onClick={() => handleStarClick(item)}>
+            <Stars size={13} isFilled={!clicked[index]} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
