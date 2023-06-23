55414ee7ad8308c0a8ff3a76f7d1ecb681faa5d7
dokimion24 <leecy3610@gmail.com>
2023-06-23 16:11:17 +0900

    feat: :sparkles: 태그이모티콘 추가, 스타일 변경

--- src/components/assets/AssetTags.tsx
+++ src/components/assets/AssetTags.tsx
@@ -1,22 +1,24 @@
+import Image from 'next/image'
+
 interface Props {
   tags: string[]
 }
 
 export default function AssetTags({ tags }: Props) {
   return (
-    <>
-      <span>icon</span>
+    <div className="item-center flex ">
+      <Image src="/icons/tag.svg" alt="tag" width={24} height={24} className="mr-2" />
       <ul>
         {tags.map((tag, i) => (
           <li
-            className="h-[2.8rem] border border-transparent-navy-second bg-neutral-navy-900 rounded
-            py-1 px-2 inline-flex items-center justify-center mr-2 mt-3"
+            className="mr-2  inline-flex h-[3.6rem] items-center
+            justify-center rounded-[0.4rem] border border-transparent-navy-second bg-neutral-navy-900 px-2 py-1"
             key={i}
           >
             {tag}
           </li>
         ))}
       </ul>
-    </>
+    </div>
   )
 }
