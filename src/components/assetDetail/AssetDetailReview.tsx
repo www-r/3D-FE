f0a22b0861c5bca8b4068687bfacbe0bb412d220
dokimion24 <leecy3610@gmail.com>
2023-06-23 13:54:57 +0900

    feat: :sparkles: 불필요한 import 삭제

--- src/components/assetDetail/AssetDetailReview.tsx
+++ src/components/assetDetail/AssetDetailReview.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
-import Star from '../common/Star'
 import CreateReview from './CreateReview'
 import { Asset } from '@/api/interface/asset'
 import ReviewList from './ReviewList'
-import { getAllReviews } from '@/api/service/review'
+import Rating from '../common/Rating'
 
 interface Props {
   asset: Asset
@@ -18,7 +17,7 @@ export default function AssetDetailReview({ asset }: Props) {
       <article className="h-full p-6 text-neutral-navy-100">
         <div className="mb-[3.2rem] flex justify-between border-b border-transparent-navy-30 py-[1.4rem]">
           <h3 className="leading-[1.936rem]">Up down run down stairs</h3>
-          <Star star={asset.rating} size="lg" />
+          <Rating star={asset.rating} size={20} />
         </div>
         <div>
           <CreateReview id={asset.assetId} />
