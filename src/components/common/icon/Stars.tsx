35ce0df407f6fe4b54ac5eeabe0be45dbacb5af1
dokimion24 <leecy3610@gmail.com>
2023-06-23 13:53:14 +0900

    feat: :sparkles: isFilled true 일 때 채워진 색 출력

--- src/components/common/icon/Stars.tsx
+++ src/components/common/icon/Stars.tsx
@@ -17,7 +17,7 @@ export default function Stars({ isFilled, size }: Props) {
       >
         <path
           d="M12 1L15.399 7.91167L23 9.02681L17.5 14.4038L18.798 22L12 18.4117L5.202 22L6.5 14.4038L1 9.02681L8.601 7.91167L12 1Z"
-          fill={isFilled ? '#C7D1DB' : '#3399FF'}
+          fill={isFilled ? '#3399FF' : '#C7D1DB'}
         />
       </svg>
     </div>
