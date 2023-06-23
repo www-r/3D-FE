1216abc1748cd85075633659c64e613ed889c7d4
dokimion24 <leecy3610@gmail.com>
2023-06-23 13:57:21 +0900

    feat: :sparkles: 리뷰 작성 훅 작성

---
+++ src/hooks/usePostReview.ts
@@ -0,0 +1,25 @@
+import { CreateReview } from '@/api/interface/review'
+import { postReview } from '@/api/service/review'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+export const usePostReview = (id: number) => {
+  const queryClient = useQueryClient()
+
+  const { mutate: createReview } = useMutation({
+    mutationFn: (review: CreateReview) => postReview(review, id),
+    onMutate: async (data) => {
+      await queryClient.cancelQueries({ queryKey: ['review', id] })
+
+      // Snapshot the previous value
+      const prevReview = queryClient.getQueryData(['review', id])
+
+      // Optimistically update to the new value
+      queryClient.setQueryData(['review', id], data)
+
+      // Return a context with the previous and new todo
+      return { prevReview, data }
+    },
+  })
+
+  return { createReview }
+}
