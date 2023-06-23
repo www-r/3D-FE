e97afa938fc8591ba3f9f321656fe2cd869c5d0a
dokimion24 <leecy3610@gmail.com>
2023-06-23 22:48:44 +0900

    feat: :sparkles: 리뷰 조건에 따라 렌더링 되도록 구현

--- src/components/assetDetail/ReviewList.tsx
+++ src/components/assetDetail/ReviewList.tsx
@@ -3,34 +3,36 @@ import { getAllReviews } from '@/api/service/review'
 import { Review, ReviewData } from '@/api/interface/review'
 import ReviewItem from './ReviewItem'
 import { useReview } from '@/hooks/useReview'
-
+import CreateReview from './CreateReview'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/store/store'
 
 interface Props {
   id: number
-  setIsWritten: (isWritten: boolean) => void
 }
 
-export default function ReviewList({ id, setIsWritten }: Props) {
+export default function ReviewList({ id }: Props) {
+  const userId = useSelector((state: RootState) => state.user.userId)
+  console.log({ userId })
+
   const { reviews } = useReview(id)
 
-  // setIsWritten()
-  //   setReviews(res.data.reviewList)
-
-  //   const reviewList = res.data.reviewList
-  //   const data = reviewList.filter((item) => item.id === userId)
+  console.log({ reviews })
 
   return (
-    <ul className="mb-[0.8rem]">
-      {reviews?.hasReview ? (
-        reviews.reviewList.map((review: Review) => (
+    <>
+      {userId && reviews?.hasAsset && !reviews?.hasReview && <CreateReview id={id} />}
+      <ul className="mb-[0.8rem]">
+        {reviews?.reviewList.map((review: Review) => (
           <ReviewItem key={review.reviewId} review={review} />
-        ))
-      ) : (
-        <section className="items-center justify-between mb-[0.8rem] flex h-[8rem] flex-col bg-neutral-navy-900 px-[1.6rem] py-[1.2rem]">
+        ))}
+      </ul>
+      {userId && !reviews?.hasAsset && !reviews?.hasReview && (
+        <section className="mb-[0.8rem] flex h-[8rem] flex-col items-center justify-between bg-neutral-navy-900 px-[1.6rem] py-[1.2rem]">
           <div className="h-[2.4rem] text-sl text-neutral-navy-200">아직 리뷰가 없습니다</div>
           <div className="text-mm text-neutral-navy-300">리뷰를 작성해보세요!</div>
         </section>
       )}
-    </ul>
+    </>
   )
 }
