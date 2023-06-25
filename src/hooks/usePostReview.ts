a424ac214d45b9e6201f8d42fa77fa808330f667
dokimion24 <leecy3610@gmail.com>
2023-06-25 11:09:39 +0900

    feat: :sparkles: editMyReview 커스텀 훅 작성

--- src/hooks/usePostReview.ts
+++ src/hooks/usePostReview.ts
@@ -1,15 +1,15 @@
-import { CreateReview, DeleteReview } from '@/api/interface/review'
-import { deleteReview, postReview } from '@/api/service/review'
+import { PostReview, DeleteReview } from '@/api/interface/review'
+import { deleteReview, editReview, postReview } from '@/api/service/review'
 import { RootState } from '@/store/store'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useSelector } from 'react-redux'
 
-export const usePostReview = (id: number) => {
+export const usePostReview = (id: number, reviewId: number = 1) => {
   const userId = useSelector((state: RootState) => state.user.userId)
   const queryClient = useQueryClient()
 
   const { mutate: createMyReview } = useMutation({
-    mutationFn: (review: CreateReview) => postReview(review, id),
+    mutationFn: (review: PostReview) => postReview(review, id),
     onMutate: async (data) => {
       await queryClient.cancelQueries({ queryKey: ['review', id] })
 
@@ -31,7 +31,7 @@ export const usePostReview = (id: number) => {
   })
 
   const { mutate: deleteMyReview } = useMutation({
-    mutationFn: (reviewId: number) => deleteReview(id, reviewId, userId),
+    mutationFn: () => deleteReview(id, reviewId, userId),
     onMutate: async (data) => {
       await queryClient.cancelQueries({ queryKey: ['review', id] })
       const prevReview = queryClient.getQueryData(['review', id])
@@ -46,5 +46,21 @@ export const usePostReview = (id: number) => {
     },
   })
 
-  return { createMyReview, deleteMyReview }
+  const { mutate: editMyReview } = useMutation({
+    mutationFn: (review: PostReview) => editReview(review, id, reviewId),
+    onMutate: async (data) => {
+      await queryClient.cancelQueries({ queryKey: ['review', id] })
+      const prevReview = queryClient.getQueryData(['review', id])
+      queryClient.setQueryData(['review', id], data)
+      return { prevReview, data }
+    },
+    onError: (_error, _data, context) => {
+      queryClient.setQueriesData(['review', id], context?.prevReview)
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['review', id])
+    },
+  })
+
+  return { createMyReview, deleteMyReview, editMyReview }
 }
