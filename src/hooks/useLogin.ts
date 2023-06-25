e917262823967459ab4fb983145bd1ed5510f109
ronieo <romelzen50@gmail.com>
2023-06-25 17:52:55 +0900

    test: :white_check_mark: 새로고침 userId 저장 이슈 테스트

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,4 +1,4 @@
-import { LoginRequest } from '@/api/interface/auth'
+import { LoginRequest, LoginResponse } from '@/api/interface/auth'
 import { login } from '@/api/service/auth'
 import { setUser } from '@/store/userSlice'
 import { setToken } from '@/utils/token'
@@ -7,15 +7,20 @@ import { AxiosError } from 'axios'
 import { useDispatch } from 'react-redux'
 
 export const useLogin = () => {
+  const queryClient = useQueryClient()
   const dispatch = useDispatch()
 
-  const { mutate } = useMutation((account: LoginRequest) => login(account), {
+  const loginMutation = useMutation<LoginResponse, AxiosError, LoginRequest>(login, {
     onSuccess: (data) => {
+      queryClient.setQueryData(['LoginData'], data.data)
       dispatch(setUser(data.data.userId))
     },
     onError: (err: AxiosError) => {
       console.log(err)
     },
   })
-  return { mutate }
+
+  return {
+    mutate: loginMutation.mutate,
+  }
 }
