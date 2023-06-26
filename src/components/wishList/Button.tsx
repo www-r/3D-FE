6b2d9d85c1b985ae2f7ec979132791c2176e8d7d
youngeun <jessicak0828@gmail.com>
2023-06-26 17:34:56 +0900

    feat: Button 컴포넌트 hover 시 색상변화 기능 구현

--- src/components/wishList/Button.tsx
+++ src/components/wishList/Button.tsx
@@ -1,20 +1,15 @@
 "use client"
-import React, { MouseEventHandler, useState } from 'react'
+import React, { MouseEventHandler } from 'react'
 
 interface Button {
-  clickHandler: Function,
+  clickHandler: MouseEventHandler,
   children:string,
 }
 
 export default function Button({ clickHandler, children}:Button) {
 
-  const [isClicked, setIsClicked] = useState(false)
-  const onClick = () => {
-    clickHandler()
-    setIsClicked(!isClicked)
-  }
+
   return (
-    
-      <button className={isClicked?'w-[12rem] h-[2.8rem] border rounded-sm text-sm text-primary-main border-primary-main' : 'w-[12rem] h-[2.8rem] border rounded-sm  text-sm  border-transparent-navy-30 text-neutral-navy-100'} onClick={onClick}>{children}</button> 
+      <button className={'w-[12rem] h-[2.8rem] border rounded-sm  text-sm  border-transparent-navy-30 text-neutral-navy-100 hover:text-primary-main hover:border-primary-main'} onClick={clickHandler}>{children}</button> 
   )
 }
