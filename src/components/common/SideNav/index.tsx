d28785d52a38cabff4c74c1928f8f2a3ac6dc622
youngeun <jessicak0828@gmail.com>
2023-06-26 17:37:18 +0900

    fix: 에러나는 부분 주석 처리

--- src/components/common/SideNav/index.tsx
+++ src/components/common/SideNav/index.tsx
@@ -4,11 +4,11 @@ import SideNavList from './SideNavList'
 import { getAllCategories } from '@/api/service/category'
 
 export default async function SideNav() {
-  const data = await getAllCategories()
+  // const data = await getAllCategories()
 
   return (
     <aside className="fixed z-10 flex h-screen w-[24.4rem] min-w-[24.4rem] flex-col justify-between border-r border-solid border-transparent-navy-30 bg-bg-1 pb-16 pt-[2.4rem] text-mm">
-      <SideNavList category={data} />
+      {/* <SideNavList category={data} /> */}
       <div className="mx-auto w-[20rem] bg-neutral-navy-900 leading-[3.8rem]">
         <Link href="#" className="flex justify-center text-neutral-navy-200">
           Neuroid&nbsp;<span className="text-primary-main">Beta</span>
