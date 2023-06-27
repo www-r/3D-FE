a4ee33960a6dd786012c7e5fc1dfdb830a91b9e0
ronieo <romelzen50@gmail.com>
2023-06-27 18:02:23 +0900

    feat: :sparkles: 위시리스트 UI 퍼블리싱
    - Icon Button 주석처리 사유: react-aria package 적용 고려

---
+++ src/components/common/icon/Icon.tsx
@@ -0,0 +1,81 @@
+import Image from 'next/image'
+import checkboxEmpty from '../../../../public/icons/checkboxOff.svg'
+import checkboxFull from '../../../../public/icons/checkboxOn.svg'
+import Cart from '../../../../public/icons/cartPlus.svg'
+import CartHover from '../../../../public/icons/cartPlusBlue.svg'
+import Trash from '../../../../public/icons/trash.svg'
+import TrashHover from '../../../../public/icons/trashBlue.svg'
+
+interface IconProps {
+  width?: number
+  height?: number
+}
+
+export const CheckboxOffIcon = ({ width = 24, height = 24 }: IconProps) => {
+  return (
+    <Image
+      src={checkboxEmpty}
+      alt="checkbox off icon"
+      width={width}
+      height={height}
+      className="flex items-center justify-center"
+    />
+  )
+}
+export const CheckboxOnIcon = ({ width = 24, height = 24 }: IconProps) => {
+  return (
+    <Image
+      src={checkboxFull}
+      alt="checkbox on icon"
+      width={width}
+      height={height}
+      className="flex items-center justify-center"
+    />
+  )
+}
+
+export const CartIcon = ({ width = 24, height = 24 }: IconProps) => {
+  return (
+    <Image
+      src={Cart}
+      alt="cart icon"
+      width={width}
+      height={height}
+      className="flex items-center justify-center"
+    />
+  )
+}
+export const BlueCartIcon = ({ width = 24, height = 24 }: IconProps) => {
+  return (
+    <Image
+      src={CartHover}
+      alt="cart icon hover"
+      width={width}
+      height={height}
+      className="flex items-center justify-center"
+    />
+  )
+}
+
+export const TrashIcon = ({ width = 24, height = 24 }: IconProps) => {
+  return (
+    <Image
+      src={Trash}
+      alt="trash icon"
+      width={width}
+      height={height}
+      className="flex items-center justify-center"
+    />
+  )
+}
+export const BlueTrashIcon = ({ width = 24, height = 24 }: IconProps) => {
+  return (
+    <Image
+      src={TrashHover}
+      alt="trash icon hover"
+      width={width}
+      height={height}
+      className="flex items-center justify-center"
+    />
+  )
+}
---
+++ src/components/wishList/TableItem.tsx
@@ -0,0 +1,63 @@
+'use client'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import { formatPrice } from '@/utils/formatPrice'
+import CustomCheckbox from '../common/CustomCheckbox'
+// import IconButton from './IconButton'
+import { BlueCartIcon, BlueTrashIcon, CartIcon, TrashIcon } from '../common/icon/Icon'
+
+interface TableItemProps {
+  assetId: number
+  image: string
+  assetName: string
+  extension: string
+  size: number
+  price: number
+  icon: React.ReactNode
+}
+
+export default function TableItem({
+  assetId,
+  image = '',
+  assetName,
+  extension,
+  size,
+  price,
+  icon,
+}: TableItemProps) {
+  const [isChecked, setIsChecked] = useState(false)
+  return (
+    <>
+      <tr className="border-b border-neutral-navy-300 ">
+        <td className="h-[10rem] py-[1rem] ">{/* <CustomCheckbox id={assetId} /> */}</td>
+        <td className="h-[10rem] py-[1rem] ">
+          <Image src={image} alt="asset" width={80} height={100} className="min-w-[8rem]" />
+        </td>
+        <td className="h-[10rem] py-[1rem] ">
+          <div className="px-[1.2rem]">
+            <h4 className="text-[2rem] font-medium">{assetName}</h4>
+            <ul className="flex flex-wrap items-center text-sm">
+              <li className="flex items-center after:m-[0.8rem] after:h-[1.2rem] after:w-[0.1rem] after:bg-transparent-navy-30">
+                {`확장자 : ${extension}`}
+              </li>
+              <li className="flex items-center after:bg-transparent-navy-30">
+                {`데이터 용량 : ${size}`}
+              </li>
+            </ul>
+          </div>
+        </td>
+        <td className="h-[10rem] py-[1rem] ">
+          <div className="px-[2.4rem] text-right text-sl">
+            <p>{formatPrice(price)}</p>
+          </div>
+        </td>
+        <td className="h-[10rem] py-[1rem]">
+          {/* <IconButton icon1={<CartIcon />} icon2={<BlueCartIcon />} /> */}
+        </td>
+        <td className="h-[10rem] py-[1rem]">
+          {/* <IconButton icon1={<TrashIcon />} icon2={<BlueTrashIcon />} /> */}
+        </td>
+      </tr>
+    </>
+  )
+}
