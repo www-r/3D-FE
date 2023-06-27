78364fa516201e1117ce02c5d8d32ddac974443e
jiyoon29 <yoon__29@naver.com>
2023-06-28 06:12:35 +0900

    feat: :sparkles: 결제 할 데이터 불러오기 입력한 데이터 보내기 위한 로직 추가

--- src/components/payment/PaymentMethod.tsx
+++ src/components/payment/PaymentMethod.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
 
-export default function PaymentMethod() {
+export default function PaymentMethod({ register, errors }: any) {
   return (
     <div className="w-[47.04%] py-[0.4rem]">
       <h3 className="border-b border-transparent-navy-30 text-sl leading-[5rem]">결제 수단</h3>
       <ul>
         <li className="mt-[2.4rem] min-h-[7.2rem] rounded-sm border border-transparent-navy-30 bg-bg-1">
-          <input type="radio" id="kakaoPay" name="pay" className="hidden" />
+          <input
+            type="radio"
+            id="kakaoPay"
+            name="pay"
+            value="카카오페이"
+            className="hidden"
+            {...register('paymentTool', { required: true })}
+          />
           <label
             htmlFor="kakaoPay"
             className="flex items-center justify-between py-[1.5rem] pl-[2.4rem] pr-[1.6rem]"
@@ -20,6 +27,9 @@ export default function PaymentMethod() {
           </label>
         </li>
       </ul>
+      {errors.paymentTool && (
+        <p className="text-sm text-point-red-text">결제 수단을 선택해주세요.</p>
+      )}
     </div>
   )
 }
