b80a858bb3e87a6eedeb0f44b8d563bbf6f7dd8f
jiyoon29 <yoon__29@naver.com>
2023-06-28 00:19:54 +0900

    feat: :sparkles: use client 사용을 위한 컴포넌트 분리

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,22 +1,10 @@
 import Layout from '@/components/common/Layout'
-import PaymentInfo from '@/components/payment/PaymentInfo'
-import OrderInfo from '@/components/payment/OrderInfo'
-import PaymentMethod from '@/components/payment/PaymentMethod'
-import Title from '@/components/common/Title'
+import Payment from '@/components/payment/Payment'
 
 export default function PaymentPage() {
   return (
     <Layout>
-      <div className="flex min-h-[calc(100vh-7.2rem)] text-neutral-navy-100">
-        <div className="w-[74.82%] px-[2.2rem] py-[6.4rem] leading-[2.6rem]">
-          <Title tit="결제" />
-          <div className="flex w-full justify-between px-[2.3rem] py-[2.4rem] font-normal">
-            <OrderInfo />
-            <PaymentMethod />
-          </div>
-        </div>
-        <PaymentInfo />
-      </div>
+      <Payment />
     </Layout>
   )
 }
---
+++ src/components/payment/Payment.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import React, { useState, useEffect } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import PaymentInfo from '@/components/payment/PaymentInfo'
+import OrderInfo from '@/components/payment/OrderInfo'
+import PaymentMethod from '@/components/payment/PaymentMethod'
+import Title from '@/components/common/Title'
+
+export default function Payment() {
+  return (
+    <div className="flex min-h-[calc(100vh-7.2rem)] text-neutral-navy-100">
+      <div className="w-[74.82%] px-[2.2rem] py-[6.4rem] leading-[2.6rem]">
+        <Title tit="결제" />
+        <div className="flex w-full justify-between px-[2.3rem] py-[2.4rem] font-normal">
+          <OrderInfo />
+          <PaymentMethod />
+        </div>
+      </div>
+      <PaymentInfo />
+    </div>
+  )
+}
