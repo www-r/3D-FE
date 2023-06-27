4af3455290fcf6d7d33d848cb2204d192772e098
jiyoon29 <yoon__29@naver.com>
2023-06-27 21:11:22 +0900

    feat: :sparkles: cart delete api 추가

--- src/api/service/cart.ts
+++ src/api/service/cart.ts
@@ -1,4 +1,5 @@
 import { axiosInstance } from '../axios'
+import { CartDeleteRequest, CartDeleteResponse } from '../interface/cart'
 
 export const cartCount = async (user: number) => {
   try {
@@ -15,5 +16,10 @@ export const cartCount = async (user: number) => {
 
 export const cartList = async (user: number) => {
   const res = await axiosInstance.get(`/s/user/${user}/cart`)
+  return res.data
+}
+
+export const cartDelete = async (T: CartDeleteRequest): Promise<CartDeleteResponse> => {
+  const res = await axiosInstance.post<CartDeleteResponse>('/s/cart/delete', T)
   return res.data
 }
