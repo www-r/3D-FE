865aa68c827f64532b5c887b3251d19de7ae45d7
jiyoon29 <yoon__29@naver.com>
2023-06-27 21:31:14 +0900

    feat: :sparkles: style 및 id 변경

--- src/components/payment/PaymentMethod.tsx
+++ src/components/payment/PaymentMethod.tsx
@@ -6,14 +6,14 @@ export default function PaymentMethod() {
     <div className="w-[47.04%] py-[0.4rem]">
       <h3 className="border-b border-transparent-navy-30 text-sl leading-[5rem]">결제 수단</h3>
       <ul>
-        <li className="mt-[2.4rem] h-[7.2rem] rounded-sm border border-transparent-navy-30 bg-bg-1">
-          <input type="radio" id="i1" className="hidden" />
+        <li className="mt-[2.4rem] min-h-[7.2rem] rounded-sm border border-transparent-navy-30 bg-bg-1">
+          <input type="radio" id="kakaoPay" name="pay" className="hidden" />
           <label
-            htmlFor="i1"
-            className="flex items-center justify-between py-[1.6rem] pl-[2.4rem] pr-[1.6rem]"
+            htmlFor="kakaoPay"
+            className="flex items-center justify-between py-[1.5rem] pl-[2.4rem] pr-[1.6rem]"
           >
             <div className="flex items-center">
-              <div className="mx-auto mr-[0.8rem] h-[3.2rem] w-[3.2rem] bg-radio-off bg-cover"></div>
+              <div className="mx-auto mr-[0.8rem] h-[3.2rem] w-[3.2rem] min-w-[3.2rem] bg-radio-off bg-cover"></div>
               <p>간편결제 : Kakao pay</p>
             </div>
             <Image src="/icons/payment-kakaoPay.svg" alt="kg" width={40} height={40} />
