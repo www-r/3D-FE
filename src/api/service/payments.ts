e93e26838a7a940a43c46e2a2429775a018a3714
jiyoon29 <yoon__29@naver.com>
2023-06-28 00:21:04 +0900

    feat: :sparkles: order api 추가

--- src/api/service/payments.ts
+++ src/api/service/payments.ts
@@ -3,9 +3,16 @@
  */
 import { readFile } from 'fs/promises'
 import path from 'path'
-import { OrderHistory } from '../interface/payment'
+import { OrderHistory, OrderResponse, PaymentPayload } from '../interface/payment'
+import { axiosInstance } from '../axios'
 
 export async function getAllOrderHistory(): Promise<OrderHistory[]> {
   const filePath = path.join(process.cwd(), 'src', 'data', 'assets.json')
   return readFile(filePath, 'utf-8').then<OrderHistory[]>(JSON.parse)
+}
+
+// 주문하기
+export const order = async (T: PaymentPayload): Promise<OrderResponse> => {
+  const res = await axiosInstance.post<OrderResponse>('/s/order', T)
+  return res.data
 }
