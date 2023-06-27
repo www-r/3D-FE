a66edd7586a9a9bdcf7a90b17fc8090fb636738c
jiyoon29 <yoon__29@naver.com>
2023-06-27 21:10:13 +0900

    feat: :sparkles: cart type 추가 생성 및 변경

--- src/api/interface/cart.ts
+++ src/api/interface/cart.ts
@@ -1,16 +1,33 @@
+import { ApiResponse } from '.'
+
 export interface CartItemProps {
-  item: {
-    cartId: number
-    asset: {
-      assetId: number
-      assetName: string
-      price: number
-      discount: number
-      discountPrice: number
-      extension: string
-      size: number
-    }
-    orderId: number | null
-    wishListId: number | null
-  }
+  item: SelectedItem
 }
+
+export interface SelectedItem {
+  cartId: number
+  asset: Asset
+  orderId: number | null
+  wishListId: number | null
+}
+
+export interface Asset {
+  assetId: number
+  assetName: string
+  price: number
+  discount: number
+  discountPrice: number
+  extension: string
+  size: number
+}
+
+export interface CartDeleteRequest {
+  userId: number
+  carts: number[]
+}
+
+export interface CartDeleteResponseData {
+  data: null
+}
+
+export type CartDeleteResponse = ApiResponse<CartDeleteResponseData>
