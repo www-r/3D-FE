68fa0ac44327daa9ecc9c989562eede9b2c3a7ae
ronieo <romelzen50@gmail.com>
2023-06-27 17:12:45 +0900

    feat: :sparkles: 마이페이지: 주문내역 필터링, 필터에 따른 주문내역 출력, 상세 에셋 Drawer 추가 로직 필요

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -3,17 +3,11 @@
  */
 import Layout from '@/components/common/Layout'
 import AccountSettingPage from './setting/page'
-import OrderHistoryPage from './orderHistory/page'
-
-// type accountInfoProps = {
-//   userId: number
-// }
 
 export default function MyPage() {
   return (
     <Layout>
       <AccountSettingPage />
-      <OrderHistoryPage />
     </Layout>
   )
 }
