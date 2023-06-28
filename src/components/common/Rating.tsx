a40486c59d353e00a54bb4067c244036a07b8b19
dokimion24 <leecy3610@gmail.com>
2023-06-29 08:03:49 +0900

    feat: :sparkles: 리뷰 별점 표시 스타일링

--- src/components/common/Rating.tsx
+++ src/components/common/Rating.tsx
@@ -7,33 +7,13 @@ interface Props {
 }
 
 export default function Rating({ star = 0, size }: Props) {
-  // const starsWidth = Math.round(starWidth * star * 100) / 100
-
-  // const emptyStars = Array.from({ length: 5 }).map((_, index) => (
-  //   <Image
-  //     key={`empty-star-${index}`}
-  //     src="/icons/emptyStar.svg"
-  //     alt="star"
-  //     width={starSize}
-  //     height={starSize}
-  //     className=""
-  //   />
-  // ))
-
-  // const filledStars = Array.from({ length: 5 }).map((_, index) => (
-  //   <Image
-  //     key={`filled-star-${index}`}
-  //     src="/icons/filledStar.svg"
-  //     alt="star"
-  //     width={starSize}
-  //     height={starSize}
-  //     style={{ minWidth: `${starWidth}rem` }}
-  //   />
-  // ))
-
   return (
-    <div className=" flex w-[20.2rem] items-center justify-between">
-      <span className="text-[2.6rem] font-bold text-primary-main">{star}</span>
+    <div
+      className={`${size < 20 ? 'w-[15.3rem]' : 'w-[20.2rem]'} flex items-center justify-between`}
+    >
+      <span className={`${size < 20 && 'text-mm'} text-[2.6rem] font-bold text-primary-main`}>
+        {star}
+      </span>
       <div className="relative flex w-[13rem]">
         <div className={`absolute left-0 top-1/2 flex -translate-y-1/2 transform`}>
           {Array.from({ length: 5 }).map((_, index) => (
