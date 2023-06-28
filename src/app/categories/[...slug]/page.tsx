c55c3daffef5e4983c61f8a772075d9080f2a03e
dokimion24 <leecy3610@gmail.com>
2023-06-29 06:46:26 +0900

    feat: :sparkles: categoryByAsset 추가

--- src/app/categories/[...slug]/page.tsx
+++ src/app/categories/[...slug]/page.tsx
@@ -1,5 +1,10 @@
-import Layout from '@/components/common/Layout';
+import CategoryByAsset from '@/components/assets/CategoryByAsset'
+import Layout from '@/components/common/Layout'
 
 export default function SubCategoriyPage() {
-  return <Layout>CategoriesDetailPage</Layout>
+  return (
+    <Layout>
+      <CategoryByAsset />
+    </Layout>
+  )
 }
