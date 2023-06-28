8dbb9499bb552ebb0ebe66f260f87d2bf0141642
dokimion24 <leecy3610@gmail.com>
2023-06-29 04:40:56 +0900

    feat: :sparkles: 카테고리 가져오는 api 함수 작성

--- src/api/service/category.ts
+++ src/api/service/category.ts
@@ -1,6 +1,6 @@
 /**
  * 카테고리 관련 API
-*/
+ */
 import { readFile } from 'fs/promises'
 import path from 'path'
 import { Category } from '@/api/interface/category'
@@ -10,13 +10,18 @@ import { Category } from '@/api/interface/category'
 //   return readFile(filePath, 'utf-8').then<Category[]>(JSON.parse)
 // }
 
+export const getAllCategories = async (): Promise<Category[]> => {
+  const filePath = path.join(process.cwd(), 'src', 'data', 'category.json')
+  return readFile(filePath, 'utf-8').then(JSON.parse)
+}
+
 /**
  * 카테고리 관련 API
  */
-import { CategoryResponse } from '@/api/interface/category'
-import { axiosInstance } from '../axios'
+// import { CategoryResponse } from '@/api/interface/category'
+// import { axiosInstance } from '../axios'
 
-export const getAllCategory = async <T = CategoryResponse>(): Promise<T> => {
-  const res = await axiosInstance.get<T>('/assets/count')
-  return res.data
-}
+// export const getAllCategory = async <T = CategoryResponse>(): Promise<T> => {
+//   const res = await axiosInstance.get<T>('/assets/count')
+//   return res.data
+// }
