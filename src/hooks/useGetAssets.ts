b6606654a34ba6f24013f35df62bcf377bae0319
dokimion24 <leecy3610@gmail.com>
2023-07-03 02:00:03 +0900

    feat: :sparkles: 카테고리 해당 에셋 가져오는 커스텀 훅 작성

--- src/hooks/useGetAssets.ts
+++ src/hooks/useGetAssets.ts
@@ -1,18 +1,22 @@
 import { getAssets, getSubCategories } from '@/api/service/asset'
 import { useQuery } from '@tanstack/react-query'
 
-export const useGetAssets = (page: number, category: string, subCategory: string) => {
+export const useGetAssets = (page: number) => {
   const { data: assets } = useQuery({
     queryKey: ['assets', page],
     queryFn: () => getAssets(page),
     keepPreviousData: true,
   })
 
-  const { data: subCategoryAssets } = useQuery({
+  return { assets }
+}
+
+export const useCategoryAssets = (page: number, category: string, subCategory: string) => {
+  const { data: categoryAssets } = useQuery({
     queryKey: ['assets', category, subCategory, page],
     queryFn: () => getSubCategories(page, category, subCategory),
     keepPreviousData: true,
   })
 
-  return { assets, subCategoryAssets }
+  return { categoryAssets }
 }
