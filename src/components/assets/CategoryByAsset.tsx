a2ae9a1bb0194ae2a9660d068d61556aa750c83c
dokimion24 <leecy3610@gmail.com>
2023-07-03 01:57:11 +0900

    feat: :sparkles: 카테고리에 해당되는 에셋 가져오는 useCategoryAssets 커스텀 훅 사용

--- src/components/assets/CategoryByAsset.tsx
+++ src/components/assets/CategoryByAsset.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react'
 import AssetList from './AssetList'
 import PaginationButton from '../common/PaginationButton'
-import { useGetAssets } from '@/hooks/useGetAssets'
+import { useCategoryAssets, useGetAssets } from '@/hooks/useGetAssets'
 import { usePathname } from 'next/navigation'
 
 export default function CategoryByAsset() {
@@ -14,22 +14,22 @@ export default function CategoryByAsset() {
   console.log(category, subCategory)
 
   const [activePage, setActivePage] = useState(0)
-  const { subCategoryAssets } = useGetAssets(activePage, category, subCategory)
-  console.log({ subCategoryAssets })
+  const { categoryAssets } = useCategoryAssets(activePage, category, subCategory)
+  console.log({ categoryAssets })
 
   return (
     <section className="min-h-[calc(100vh-12.3rem)] px-[2.3rem] pt-8">
       <div className="w-full">
         <div className=" mb-[2.5rem] h-[3rem] text-neutral-navy-100">
           <span className="text-lg font-bold leading-[3rem]">{subCategory} </span>
-          <span className="text-sl">({subCategoryAssets?.data?.totalElement})</span>
+          <span className="text-sl">({categoryAssets?.data?.totalElement})</span>
         </div>
       </div>
-      <AssetList assets={subCategoryAssets?.data?.assetList} />
+      <AssetList assets={categoryAssets?.data?.assetList} />
       <PaginationButton
         activePage={activePage}
         setActivePage={setActivePage}
-        pages={subCategoryAssets?.data?.totalPage}
+        pages={categoryAssets?.data?.totalPage}
       />
     </section>
   )
