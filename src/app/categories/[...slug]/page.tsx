9ef8e86bd690bba79752f7441dd87070b59b4582
dokimion24 <leecy3610@gmail.com>
2023-07-04 19:53:23 +0900

    feat: :sparkles: 카테고리 페이지 Drawer적용

--- src/app/categories/[...slug]/page.tsx
+++ src/app/categories/[...slug]/page.tsx
@@ -1,10 +1,19 @@
+import AssetDetail from '@/components/assetDetail/AssetDetail'
 import CategoryByAsset from '@/components/assets/CategoryByAsset'
+import Drawer from '@/components/common/Drawer'
 import Layout from '@/components/common/Layout'
 
 export default function SubCategoriyPage() {
   return (
     <Layout>
-      <CategoryByAsset />
+      <div className="flex">
+        <div className="h-[100%] w-full">
+          <CategoryByAsset />
+        </div>
+        <Drawer>
+          <AssetDetail />
+        </Drawer>
+      </div>
     </Layout>
   )
 }
